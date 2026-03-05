bdc22430a34f022edb93671b7abb6c6627e951ce
akon9293 <mohibuddinakon@gmail.com>
2026-03-05 23:57:22 +0600

    function2

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-function max(arr) {
-    let largest = arr[0];
+// function max(arr) {
+//     let largest = arr[0];
 
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > largest) {
-            largest = arr[i];
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i] > largest) {
+//             largest = arr[i];
+//         }
+//     }
+
+//     return largest;
+// }
+
+// let number = [56,45,100,44,152,1];
+// let result = max(number); 
+// console.log(result);
+
+
+function getEvenNumber(arr) {
+
+    let evenNumbers = [];
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] % 2 === 0) {
+            evenNumbers.push(arr[i]);
         }
+
     }
 
-    return largest;
+    return evenNumbers;
 }
 
-let number = [56,45,100,44,152,1];
-let result = max(number); 
+let number = [1,2,3,4,5,6];
+let result = getEvenNumber(number);
+
 console.log(result);
